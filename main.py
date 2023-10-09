e2683c8c10269d63413403688b2995dca4eecb8a
Wil Wang <wil.wang@gmail.com>
2023-10-09 20:16:22 +0000

    modified to set quota_project_id on credentials

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from google.cloud import aiplatform
-from vertexai.preview.language_models import TextGenerationModel
+import google
+import vertexai
 
-import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'creds/<name of your service account key>.json'
-
-generation_model = TextGenerationModel.from_pretrained("text-bison@latest")
-
-PROJECT_ID = "<PROJECT ID>"
+PROJECT_ID = "<PROJECT_ID>"
 LOCATION = "<REGION>"
 
-import vertexai
-vertexai.init(project=PROJECT_ID, location=LOCATION)
+# need to set the quota_project_id (https://github.com/googleapis/python-aiplatform/issues/2557#issuecomment-1709284744)
+creds, _ = google.auth.load_credentials_from_file(
+    filename='./creds/<service account key file>.json', 
+    quota_project_id=PROJECT_ID)
 
-txt = generation_model.predict("""
-Why is the sky blue?
-""")
+vertexai.init(
+    project=PROJECT_ID, 
+    location=LOCATION, 
+    credentials=creds)
+
+from vertexai.preview.language_models import TextGenerationModel
+
+generation_model = TextGenerationModel.from_pretrained("text-bison@latest")
+txt = generation_model.predict("""Why is the sky blue?""")
 
 print(txt)
